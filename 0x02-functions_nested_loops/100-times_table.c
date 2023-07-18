383eedc95bf2e6dadc2c50978aef99b66bf3627f
Yannick <pagnilonda@gmail.com>
2023-07-18 17:17:30 +0000

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,11 +6,13 @@
  */
 void print_times_table(int n)
 {
+	int i, j, k;
+
 	if (n < 0 || n > 15)
 	{
-		for (int i = 0; i <= n; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for (int j = 0; j <= n; j++)
+			for (j = 0; j <= n; j++)
 			{
 				int k = j * i;
 
