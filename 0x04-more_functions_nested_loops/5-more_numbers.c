c11cbf77ad414eb7362171edb6cabfa6771f5d29
Yannick <pagnilonda@gmail.com>
2023-07-20 18:11:49 +0000

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - print more numbers
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+				_putchar('1');
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
