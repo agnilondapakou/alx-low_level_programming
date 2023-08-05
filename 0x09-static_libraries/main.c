c034825d11a8759c976c44e88d478fd1e63e353b
Yannick <pagnilonda@gmail.com>
2023-08-05 00:55:06 +0000

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  *main - print string
+  *
+  *Return: 0 for success
+  */
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t");
+	return (0);
+}
