081d2eaf5ed7a546a448b572ba2cb83abc44e59d
Yannick <pagnilonda@gmail.com>
2023-08-14 17:10:26 +0000

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - a dog basic informations
+  * @name: first member
+  * @age: seconde member
+  * @owner: third member
+  *
+  * Description: more description
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
